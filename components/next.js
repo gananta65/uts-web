c9bf8c3463056878e5e74ca5d6ed0d72530fab21
gananta65 <gananta65@gmail.com>
2021-11-15 10:52:22 +0800

    edit carousel

--- components/next.js
+++ components/next.js
@@ -1,10 +1,12 @@
 app.component('comingsoon', {
     template: 
     /*html*/
-    `<div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+    `<div>
+    <div id="carouselExampleControls">
     <div class="carousel-inner">
       <div class="carousel-item active">
         <img class="d-block w-100" src="./assets/images/download.jpg" alt="Coming Soon">
       </div>
+  </div>
   </div>`
 })
